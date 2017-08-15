80910a10fc5cdd4e9580cc50eeac896b09cf83b4
Ralf Zeiler <rze@dreif.com>
2017-08-15 17:13:42 +0200

    change dist

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,28 @@
 module.exports = {
-  entry: [
-    './src/index.js'
-  ],
+  entry: ['./src/index.js'],
   devtool: 'eval',
   module: {
-    loaders: [{
-      test: /\.js?$/,
-      exclude: /node_modules/,
-      loader: 'babel-loader',
-      query: {
-        plugins: ['transform-runtime'],
-        presets: ['es2015', 'stage-2', 'react']
-      },
-    },
-    {
-      test: /\.css$/,
-      loader: 'style-loader!css-loader'
-    },
-    {
-      test: /\.(eot|svg|ttf|woff|woff2)$/,
-      loader: 'file-loader?name=fonts/[name].[ext]'
-    },
-    {
-      test: /\.jpg$/,
-      loader: 'file-loader?name=img/[name].[ext]'
-    }]
+    loaders: [
+      {
+        test: /\.js?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          plugins: ['transform-runtime'],
+          presets: ['es2015', 'stage-2', 'react']
+        }
+      }, {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader'
+      }, {
+        test: /\.(png|jpg|gif|eot|svg|ttf|woff|woff2)$/,
+        use: [
+          {
+            loader: 'url-loader'
+          }
+        ]
+      }
+    ]
   },
   resolve: {
     extensions: ['.js']
